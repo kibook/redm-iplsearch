630c8c14ce9b2d28027ea010e20b6b94628e23f5
kibukj <kibuzhj@gmail.com>
2021-02-01 20:03:15 -0500

    Include imap name

--- client.lua
+++ client.lua
@@ -1,23 +1,30 @@
-function IsPositionInsideImapStreamingExtents(imap, x, y, z)
-	return Citizen.InvokeNative(0x73B40D97D7BAAD77, imap, x, y, z)
-end
+RegisterCommand("imapsearch", function(source, args, raw)
+	local radius = (args[1] and tonumber(args[1])) or 10.0
 
-RegisterCommand('imapsearch', function(source, args, raw)
 	CreateThread(function()
 		local coords = GetEntityCoords(PlayerPedId())
 
-		print(string.format('Started searching for Imaps at %.2f, %.2f, %.2f ...', coords.x, coords.y, coords.z))
+		print(string.format("Started search at %.2f, %.2f, %.2f ...", coords.x, coords.y, coords.z))
 
 		local imaps = {}
+
 		for imap, info in pairs(Imaps) do
-			table.insert(imaps, {hash = imap, x = info.x, y = info.y, z = info.z})
+			table.insert(imaps, {
+				hash = imap,
+				hashname = info.hashname,
+				x = info.x,
+				y = info.y,
+				z = info.z
+			})
 		end
 
 		for i = 1, #imaps do
 			local distance = #(coords - vector3(imaps[i].x, imaps[i].y, imaps[i].z))
 
-			if distance <= 10.0 then
-				print(imaps[i].hash, IsImapActive(imaps[i].hash) and 'active' or 'deactive')
+			if distance <= radius then
+				local status = IsImapActive(imaps[i].hash) and "active" or "inactive"
+
+				print(string.format("%10d %8s %s", imaps[i].hash, status, imaps[i].hashname))
 			end
 
 			if i % 100 == 0 then
@@ -25,6 +32,12 @@ RegisterCommand('imapsearch', function(source, args, raw)
 			end
 		end
 
-		print('Done')
+		print("Done")
 	end)
+end)
+
+CreateThread(function()
+	TriggerEvent("chat:addSuggestion", "/imapsearch", "Search for nearby imaps", {
+		{name = "radius", help = "Radius around you to search (default: 10.0)"}
+	})
 end)
